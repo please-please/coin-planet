12e51a858395e232037776025099c5dd43d8d17e
JooYoung2274 <jooooo2274@gmail.com>
2024-04-19 21:02:52 +0900

    제발..

--- main/background.ts
+++ main/background.ts
@@ -5,7 +5,10 @@ import { createWindow } from './helpers';
 import * as fs from 'fs';
 import axios from 'axios';
 import { getCoinPrice, orderCoin } from '../renderer/api/api';
-
+import { v4 } from 'uuid';
+import { sign } from 'jsonwebtoken';
+import * as queryEncode from 'querystring';
+import crypto from 'crypto';
 const isProd = process.env.NODE_ENV === 'production';
 
 const currentPrice = {
@@ -76,28 +79,28 @@ if (isProd) {
     width: 1000,
     height: 600,
   });
-  // const userDataFilePath = `${__dirname}/private_user_data.json`;
-  // const userDataFile = fs.readFileSync(userDataFilePath, 'utf8');
-  // const userData = JSON.parse(userDataFile);
-  // if (userData.accessKey === '' || userData.secretKey === '') {
-  if (isProd) {
-    await mainWindow.loadURL('app://./apply');
+  const userDataFilePath = `${__dirname}/private_user_data.json`;
+  const userDataFile = fs.readFileSync(userDataFilePath, 'utf8');
+  const userData = JSON.parse(userDataFile);
+  if (userData.accessKey === '' || userData.secretKey === '') {
+    if (isProd) {
+      await mainWindow.loadURL('app://./apply');
+    } else {
+      const port = process.argv[2];
+      await mainWindow.loadURL(`http://localhost:${port}/apply`);
+      mainWindow.webContents.openDevTools();
+    }
   } else {
-    const port = process.argv[2];
-    await mainWindow.loadURL(`http://localhost:${port}/apply`);
-    mainWindow.webContents.openDevTools();
+    if (isProd) {
+      await mainWindow.loadURL('app://./home');
+    } else {
+      const port = process.argv[2];
+      await mainWindow.loadURL(`http://localhost:${port}/home`);
+      mainWindow.webContents.openDevTools();
+    }
   }
-  // }
 
   // intervalPrice();
-
-  if (isProd) {
-    await mainWindow.loadURL('app://./home');
-  } else {
-    const port = process.argv[2];
-    await mainWindow.loadURL(`http://localhost:${port}/home`);
-    mainWindow.webContents.openDevTools();
-  }
 })();
 
 app.on('window-all-closed', () => {
@@ -167,4 +170,35 @@ ipcMain.on('orderReservation', (evt, arg) => {
   const dataFilePath = `${__dirname}/reservation_order_data.json`;
   const reservationOrderData = JSON.stringify(arg, null, 2);
   fs.writeFileSync(dataFilePath, reservationOrderData, 'utf8');
+});
+
+ipcMain.on('getToken', async (evt, arg) => {
+  const dataFilePath = `${__dirname}/private_user_data.json`;
+  const userDataFile = fs.readFileSync(dataFilePath, 'utf8');
+  const userData = JSON.parse(userDataFile);
+  if (arg.body === undefined) {
+    const payload = {
+      access_key: userData.accessKey,
+      nonce: v4(),
+    };
+
+    const token = sign(payload, userData.secretKey);
+
+    evt.sender.send('tokenReturn', { status: 'success', token });
+  }
+  const query = queryEncode.encode(arg.body);
+
+  const hash = crypto.createHash('sha512');
+  const queryHash = hash.update(query, 'utf-8').digest('hex');
+
+  const payload = {
+    access_key: userData.accessKey,
+    nonce: v4(),
+    query_hash: queryHash,
+    query_hash_alg: 'SHA512',
+  };
+
+  const token = sign(payload, userData.secretKey);
+
+  evt.sender.send('tokenReturn', { status: 'success', token });
 });
