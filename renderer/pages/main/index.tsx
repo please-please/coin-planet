65399bbfbd96627439e6040c74872059121b3d6f
2pandi <coder.2pandi@gmail.com>
2024-04-22 15:30:32 +0900

    main 페이지 전일대비손익까지

--- renderer/pages/main/index.tsx
+++ renderer/pages/main/index.tsx
@@ -1,100 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TableColumnsType, Table } from 'antd';
 import { useRecoilState } from 'recoil';
 import { MyAssets, MyReservations, MyUserData } from '../../recoil/atom';
 import electron from 'electron';
+import { getCoinPrice } from '../../api/api';
+import { I_tickerData } from '../../api/interface';
+import { coinList } from '../../constants/coinList';
 
 const ipcRenderer = electron.ipcRenderer;
 
 const isProd: boolean = process.env.NODE_ENV === 'production';
 
-interface DataType {
+interface I_tableData {
   key: React.Key;
-  name: string;
-  age: number;
-  address: string;
+  market: string;
+  profitLossComparedPreviousDay?: number;
+  profitLoss1st?: number;
+  profitLoss2nd?: number;
+  profitLoss3rd?: number;
+  totalProfitLoss?: number;
 }
 
-const columns: TableColumnsType<DataType> = [
-  {
-    title: '종목이름',
-    width: 100,
-    dataIndex: 'name',
-    fixed: 'left',
-  },
-  {
-    title: 'Age',
-    width: 100,
-    dataIndex: 'age',
-  },
-  { title: 'Column 1', dataIndex: 'address', fixed: 'left' },
-  { title: 'Column 2', dataIndex: 'address' },
-  { title: 'Column 3', dataIndex: 'address' },
-  { title: 'Column 4', dataIndex: 'address' },
-  { title: 'Column 5', dataIndex: 'address' },
-  { title: 'Column 6', dataIndex: 'address' },
-  { title: 'Column 7', dataIndex: 'address' },
-  { title: 'Column 8', dataIndex: 'address' },
-  {
-    title: 'Action 1',
-    fixed: 'right',
-    width: 90,
-    render: () => <a>action</a>,
-  },
-  {
-    title: 'Action 2',
-    width: 90,
-    render: () => <a>action</a>,
-  },
-  {
-    title: 'Action 3',
-    fixed: 'right',
-    width: 90,
-    render: () => <a>action</a>,
-  },
-];
-
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York Park',
-  },
-];
-
 function Main() {
+  const [tickerData, setTickerData] = useState<I_tickerData[]>();
+  const [tableData, setTableData] = useState<I_tableData[]>(TABLE_DEFAULT_DATA);
+  const [columns, setColumns] = useState<TableColumnsType<I_tableData>>(TABLE_DEFAULT_COLUMNS);
   const [myUserData, setMyUserData] = useRecoilState(MyUserData);
   const [myAssets, setMyAssets] = useRecoilState(MyAssets);
   const [myReservationOrderData, setMyReservationOrderData] = useRecoilState(MyReservations);
 
-  useEffect(() => {
-    // 저장되어있는 private user data 불러와서 글로벌에 할당해야함. recoil 쓸까 말까
+  // useEffect(() => {
+  //   // 저장되어있는 private user data 불러와서 글로벌에 할당해야함. recoil 쓸까 말까
 
-    ipcRenderer.send('getSavedUserDataFile', {});
-    ipcRenderer.on('reply', (evt, arg) => {
-      if (arg.status === 'success') {
-        setMyUserData(arg.userData);
-        console.log(arg.userData);
-      }
-    });
+  //   ipcRenderer.send('getSavedUserDataFile', {});
+  //   ipcRenderer.on('reply', (evt, arg) => {
+  //     if (arg.status === 'success') {
+  //       setMyUserData(arg.userData);
+  //       console.log(arg.userData);
+  //     }
+  //   });
 
-    ipcRenderer.send('getSavedAssetsDataFile', {});
-    ipcRenderer.on('assetsReturn', (evt, arg) => {
-      if (arg.status === 'success') {
-        setMyAssets(arg.assetsData);
-      }
-      console.log(arg.assetsData);
-    });
+  //   ipcRenderer.send('getSavedAssetsDataFile', {});
+  //   ipcRenderer.on('assetsReturn', (evt, arg) => {
+  //     if (arg.status === 'success') {
+  //       setMyAssets(arg.assetsData);
+  //     }
+  //     console.log(arg.assetsData);
+  //   });
 
-    ipcRenderer.send('getSavedReservationOrderDataFile', {});
-    ipcRenderer.on('reservationOrderReturn', (evt, arg) => {
-      if (arg.status === 'success') {
-        setMyReservationOrderData(arg.reservationOrderData);
-      }
-      console.log(arg.reservationOrderData);
-    });
-  }, []);
+  //   ipcRenderer.send('getSavedReservationOrderDataFile', {});
+  //   ipcRenderer.on('reservationOrderReturn', (evt, arg) => {
+  //     if (arg.status === 'success') {
+  //       setMyReservationOrderData(arg.reservationOrderData);
+  //     }
+  //     console.log(arg.reservationOrderData);
+  //   });
+  // }, []);
 
   const reload = () => {
     // 체결내역 불러와서
@@ -122,31 +83,60 @@ function Main() {
   //   }
   // };
 
+  useEffect(() => {
+    getCoinPrice()
+      .then((res) => {
+        console.log(res);
+        setTickerData(res.data);
+      })
+      .catch(() => alert('조회 오류!'));
+  }, []);
+
+  useEffect(() => {
+    if (tickerData) {
+      const newTableData = tableData.map((item, i) => {
+        const { prev_closing_price, change, change_price } = tickerData[i];
+        const yinyang = change === 'FALL' ? -1 : 1;
+        return {
+          ...item,
+          profitLossComparedPreviousDay: +((change_price / prev_closing_price) * yinyang * 100).toFixed(2),
+        };
+      });
+
+      setTableData(newTableData);
+    }
+  }, [tickerData]);
+
   return (
     <React.Fragment>
       {/* <Header>
         <a className="text-white">리스트</a>
       </Header> */}
 
-      <Table columns={columns} dataSource={data} scroll={{ x: 1300 }} pagination={false} bordered />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          marginTop: '50px',
-        }}
-      >
-        {/* <Button size={'large'} onClick={handleApplyPage}>
-          key 등록
-        </Button>
-        <Button size={'large'} onClick={handleSelectPage}>
-          주문
-        </Button> */}
-      </div>
+      <Table columns={columns} dataSource={tableData} scroll={{ x: 1300 }} pagination={false} bordered />
     </React.Fragment>
   );
 }
 
 export default Main;
+
+const TABLE_DEFAULT_DATA: I_tableData[] = coinList.map((v) => ({ key: v.key, market: v.market }));
+
+const TABLE_DEFAULT_COLUMNS: TableColumnsType<I_tableData> = [
+  {
+    title: '종목이름',
+    dataIndex: 'market',
+    fixed: 'left',
+  },
+  {
+    title: '전일대비 손익',
+    dataIndex: 'profitLossComparedPreviousDay',
+    className: 'numeric_value',
+    render: (number) => (number ? <p style={{ color: `${number > 0 ? 'red' : 'blue'}` }}>{`${number}%`}</p> : null),
+  },
+  { title: '1차수 손익', dataIndex: 'profitLoss1st', className: 'numeric_value' },
+  { title: '2차수 손익', dataIndex: 'profitLoss2nd', className: 'numeric_value' },
+  { title: '3차수 손익', dataIndex: 'profitLoss3rd', className: 'numeric_value' },
+  { title: '전체 손익', dataIndex: 'totalProfitLoss', className: 'numeric_value' },
+  { title: '전량 매도', dataIndex: 'sellAll', fixed: 'right', render: () => <a>전량 매도</a> },
+];
