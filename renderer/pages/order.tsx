1a9273fca8905a1ff610ef7a9e9af7dd1582baf8
2pandi <coder.2pandi@gmail.com>
2024-06-04 16:25:38 +0900

    order rate input 추가

--- renderer/pages/order.tsx
+++ renderer/pages/order.tsx
@@ -27,14 +27,15 @@ function Order() {
   const [loading, setLoading] = useState<{ [key: string]: boolean }>({ order: false, reload: false });
   const [coinListData, setCoinListData] = useState<I_coinData[]>(coinList);
   const [orderData, setOrderData] = useState<I_orderData>({
-    limit: 1,
+    limit: 0,
     market: '',
     side: 'bid',
     ord_type: 'limit',
-    askingRate: 5,
-    biddingRate: 5,
+    askingRate: 0,
+    biddingRate: 0,
     inputPrice: 0,
   });
+  const [isValidOrderInput, setIsValidOrderInput] = useState<boolean>(false);
 
   const hasAsk = useRecoilValue<I_hasAsk>(HasAsk);
 
@@ -69,46 +70,6 @@ function Order() {
       if (router.asPath !== '/apply') router.push('/apply');
     }, secondsToGo * 1000);
   };
-
-  // 기존에 종목별 매매 예약 내역이 있는지 조회
-  useGetReservationOrderData();
-
-  // 주문 실행시 데이터 조회
-  useGetPurchaseData();
-
-  // 주문 실행시 주문 데이터 JSON 저장
-  useSaveOrderData({
-    [orderData.market]: {
-      bid: [
-        {
-          inputPrice: orderData.inputPrice,
-          ord_type: orderData.ord_type,
-          biddingRate: orderData.biddingRate,
-          askingRate: orderData.askingRate,
-        },
-      ],
-      ask: [],
-      limit: orderData.limit,
-    },
-  });
-
-  // 등록된 API key가 없는 경우 등록 페이지로 리다이렉트
-  useEffect(() => {
-    getToken(countDown);
-  }, []);
-
-  // 실시간 코인 종목별 가격 조회
-  useEffect(() => {
-    if (coinPrice.tickerData?.length) {
-      const coinPriceList = coinPrice.tickerData.map((coinPriceData) => coinPriceData.trade_price);
-
-      setCoinListData(
-        coinList.map((e, i) => {
-          return { ...e, price: coinPriceList[i].toLocaleString() };
-        }),
-      );
-    }
-  }, [coinPrice.tickerData]);
 
   const reload = () => {
     setLoading((pre) => ({ ...pre, reload: true }));
@@ -153,6 +114,14 @@ function Order() {
     setOrderData({ ...orderData, inputPrice: Number(e.target.value) });
   };
 
+  const onBiddingRateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrderData({ ...orderData, biddingRate: Number(e.target.value) });
+  };
+
+  const onAskingRateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrderData({ ...orderData, askingRate: Number(e.target.value) });
+  };
+
   const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
     setSelectedRowKeys(newSelectedRowKeys);
     setOrderData({ ...orderData, market: coinListData[Number(newSelectedRowKeys[0]) - 1]?.market ?? '' });
@@ -162,28 +131,74 @@ function Order() {
     selectedRowKeys,
     onChange: onSelectChange,
   };
-  const hasSelected = selectedRowKeys.length > 0;
+
+  // 기존에 종목별 매매 예약 내역이 있는지 조회
+  useGetReservationOrderData();
+
+  // 주문 실행시 데이터 조회
+  useGetPurchaseData();
+
+  // 주문 실행시 주문 데이터 JSON 저장
+  useSaveOrderData({
+    [orderData.market]: {
+      bid: [
+        {
+          inputPrice: orderData.inputPrice,
+          ord_type: orderData.ord_type,
+          biddingRate: orderData.biddingRate,
+          askingRate: orderData.askingRate,
+        },
+      ],
+      ask: [],
+      limit: orderData.limit,
+    },
+  });
+
+  // 등록된 API key가 없는 경우 등록 페이지로 리다이렉트
+  useEffect(() => {
+    getToken(countDown);
+  }, []);
+
+  // 실시간 코인 종목별 가격 조회
+  useEffect(() => {
+    if (coinPrice.tickerData?.length) {
+      const coinPriceList = coinPrice.tickerData.map((coinPriceData) => coinPriceData.trade_price);
+
+      setCoinListData(
+        coinList.map((e, i) => {
+          return { ...e, price: coinPriceList[i].toLocaleString() };
+        }),
+      );
+    }
+  }, [coinPrice.tickerData]);
+
+  useEffect(() => {
+    if (!Object.values(orderData).filter((v) => !v).length) {
+      setIsValidOrderInput(true);
+    }
+  }, [orderData]);
+
   return (
     <React.Fragment>
       <Typography.Title level={2}>주문하기</Typography.Title>
-      <div>
-        <Button style={{ float: 'right', height: '50px' }} type="primary" onClick={reload} loading={loading.reload}>
-          새로고침
-        </Button>
-        <Button
-          style={{ float: 'right', height: '50px', marginRight: '10px' }}
-          type="primary"
-          onClick={order}
-          disabled={!hasSelected}
-          loading={loading.order}
-        >
+      <div className="order_area">
+        <div className="order_options">
+          <Input placeholder="구매금액" onChange={onPriceChange}></Input>
+          <Input placeholder="차수입력" onChange={onLimitChange}></Input>
+          <Input placeholder="자동매수 하락율(%)" onChange={onBiddingRateChange}></Input>
+          <Input placeholder="자동매도 증가율(%)" onChange={onAskingRateChange}></Input>
+        </div>
+        <Button type="primary" onClick={order} disabled={!isValidOrderInput} loading={loading.order}>
           주문하기
         </Button>
-        <Input style={{ marginTop: '1rem' }} placeholder="구매금액" onChange={onPriceChange}></Input>
-        <Input style={{ marginTop: '0.5rem' }} placeholder="차수입력" onChange={onLimitChange}></Input>
+      </div>
+      <div className="order_buttons">
+        <Button style={{ float: 'right', height: '50px' }} type="default" onClick={reload} loading={loading.reload}>
+          새로고침
+        </Button>
       </div>
       <Table
-        style={{ marginTop: '2rem' }}
+        style={{ marginTop: '1rem' }}
         rowSelection={rowSelection}
         columns={columns}
         dataSource={coinListData}
