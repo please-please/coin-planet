9962e1de4348cc13d8bafe7cd19ada3732449110
JooYoung2274 <jooooo2274@gmail.com>
2024-12-24 11:13:00 +0900

    리스트 수정

--- main/coinList.ts
+++ main/coinList.ts
@@ -1,32 +1,22 @@
 export const coinList = [
   {
-    key: '1',
     name: '비트코인',
     market: 'KRW-BTC',
-    price: 0,
   },
   {
-    key: '2',
     name: '이더리움',
     market: 'KRW-ETH',
-    price: 0,
   },
   {
-    key: '3',
     name: '리플',
     market: 'KRW-XRP',
-    price: 0,
   },
   {
-    key: '4',
     name: '도지',
     market: 'KRW-DOGE',
-    price: 0,
   },
   {
-    key: '5',
     name: '솔라나',
     market: 'KRW-SOL',
-    price: 0,
   },
 ];
